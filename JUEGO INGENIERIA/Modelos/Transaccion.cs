76c12f703597c2e516f2c8dc43468dfed82a1c0c
grism13 <gmartinez.7707@unimar.edu.ve>
2026-02-15 18:25:32 -0400

    planteando todos los modulos y archivos

---
+++ JUEGO INGENIERIA/Modelos/Transaccion.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JUEGO_INGENIERIA.Modelos
+{
+    internal class Transaccion
+    {
+    }
+}
---
+++ JUEGO INGENIERIA/Vistas/FormNivel1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace JUEGO_INGENIERIA.Vistas
+{
+    public partial class FormNivel1 : Form
+    {
+        public FormNivel1()
+        {
+            InitializeComponent();
+        }
+    }
+}
