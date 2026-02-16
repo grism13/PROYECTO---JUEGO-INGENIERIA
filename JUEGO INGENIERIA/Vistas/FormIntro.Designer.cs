cae99364dab3f864df95f1f389bbf4957132ab37
grism13 <gmartinez.7707@unimar.edu.ve>
2026-02-16 16:18:58 -0400

    ajaaa

--- JUEGO INGENIERIA/Vistas/FormIntro.Designer.cs
+++ JUEGO INGENIERIA/Vistas/FormIntro.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormIntro";
+            SuspendLayout();
+            // 
+            // FormIntro
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(1357, 669);
+            ForeColor = Color.Black;
+            Name = "FormIntro";
+            Text = "HISTORIA";
+            ResumeLayout(false);
         }
 
         #endregion
