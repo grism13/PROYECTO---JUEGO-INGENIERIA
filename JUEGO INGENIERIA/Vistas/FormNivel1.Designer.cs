ecc181238c6ef21a46d2743e8cfdc1bafc637239
eliezer230 <erodrguez.6801@unimar.edu.ve>
2026-02-17 08:28:14 -0400

    Marcel Ruiz

--- JUEGO INGENIERIA/Vistas/FormNivel1.Designer.cs
+++ JUEGO INGENIERIA/Vistas/FormNivel1.Designer.cs
@@ -28,18 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
+            pbOswald = new PictureBox();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pbOswald).BeginInit();
             SuspendLayout();
+            // 
+            // pbOswald
+            // 
+            pbOswald.Location = new Point(0, 258);
+            pbOswald.Name = "pbOswald";
+            pbOswald.Size = new Size(396, 397);
+            pbOswald.SizeMode = PictureBoxSizeMode.Zoom;
+            pbOswald.TabIndex = 0;
+            pbOswald.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(507, 640);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 20);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
             // 
             // FormNivel1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1117, 658);
+            BackColor = SystemColors.WindowFrame;
+            ClientSize = new Size(1552, 790);
+            Controls.Add(label1);
+            Controls.Add(pbOswald);
             Name = "FormNivel1";
             Text = "FormNivel1";
+            ((System.ComponentModel.ISupportInitialize)pbOswald).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox pbOswald;
+        private Label label1;
     }
 }
