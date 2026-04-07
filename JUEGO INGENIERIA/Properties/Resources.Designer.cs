61b0276d3cb9d01e3f0bbfbde8d7d22377a9b155
eliezer230 <erodrguez.6801@unimar.edu.ve>
2026-04-06 22:47:13 -0400

    colocando cambios

--- JUEGO INGENIERIA/Properties/Resources.Designer.cs
+++ JUEGO INGENIERIA/Properties/Resources.Designer.cs
@@ -231,6 +231,16 @@ namespace JUEGO_INGENIERIA.Properties {
         }
         
         /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap btnTitulo {
+            get {
+                object obj = ResourceManager.GetObject("btnTitulo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Busca una cadena traducida similar a .
         /// </summary>
         internal static string cancion {
